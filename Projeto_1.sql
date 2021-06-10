3198194b944b911e93608bf610833e1889306031
David W. de Jesus <80333501+davidwjdev@users.noreply.github.com>
2021-06-10 20:15:12 -0300

    Add create database and create table

--- Projeto_1.sql
+++ Projeto_1.sql
@@ -1,1 +1,9 @@
-davidwjdev/Projeto_1_SQL
+create database projeto_1;
+use projeto_1;
+create table transacao (
+id int not null auto_increment primary key,
+tipo varchar(50),
+nome varchar(200),
+valor float(10,2),
+data_cadastro date);
+
